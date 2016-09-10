afd774766dc18c9fc449b77478872bd59a143f16
punitha25 <np.punitha@gmail.com>
2016-09-10 21:58:35 +0530

    Update and rename .gitignore to odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("\n enter the number");
+scanf("%d",&a);
+if(n%2==0)
+{
+printf("\n the number is even");
+else
+printf("\n the number is odd");
+}
+return 0;
+}
